172f0bc0855e0a8527de484c137feabc500c32bf
Hadi Roudbari <hadiroudbari@gmail.com>
2023-07-21 18:13:27 +0430

    Edit and Tags

--- assets/js/addNote.js
+++ assets/js/addNote.js
@@ -1,5 +1,8 @@
 const submitBtn = document.querySelector("#submit");
 const form = document.querySelector("#form");
+const title = document.querySelector("#title");
+const textarea = document.querySelector("#text");
+const tag = document.querySelector("#tag");
 
 const generateToast = (text, color, duration, callback) => {
   Toastify({
@@ -27,7 +30,20 @@ submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
   const formData = Object.fromEntries(new FormData(form).entries());
-  if (formData.text && formData.title) {
+
+  if (hasSelectedNote()) {
+    editSelectedNote(formData);
+    form.reset();
+    generateToast(
+      "Note edited successfully",
+      "linear-gradient(to right, #11998e, #38ef7d)",
+      1000,
+      () => location.assign("./notes.html")
+    );
+    return;
+  }
+
+  if (formData.text && formData.title && formData.tag) {
     formData.id = Date.now();
     const getNotes = getLocalStorage();
     const notes = getNotes ? getNotes : [];
@@ -49,3 +65,40 @@ submitBtn.addEventListener("click", function (e) {
     );
   }
 });
+
+// ----------
+// EDIT NOTES
+// ----------
+
+const hasSelectedNote = function () {
+  const editedNote = JSON.parse(localStorage.getItem("selectedNote"));
+  return editedNote;
+};
+
+const showSelectedNote = function () {
+  const selectedNote = hasSelectedNote();
+
+  if (selectedNote) {
+    title.value = selectedNote.title;
+    textarea.value = selectedNote.text;
+    tag.value = selectedNote.tag;
+  }
+};
+
+const editSelectedNote = function (formData) {
+  const selectedNote = hasSelectedNote();
+  const notes = getLocalStorage();
+
+  notes.forEach((item) => {
+    if (item.id === selectedNote.id) {
+      item.title = formData.title;
+      item.text = formData.text;
+      item.tag = formData.tag;
+    }
+  });
+
+  localStorage.removeItem("selectedNote");
+  setLocalStorage(notes);
+};
+
+document.addEventListener("DOMContentLoaded", showSelectedNote);
--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,5 +1,6 @@
 const noNotes = document.querySelector(".null__notes");
 const noteBox = document.querySelector(".cards__box");
+const tagList = document.querySelector(".tags__list");
 
 const generateNotes = function (notes) {
   noteBox.innerHTML = "";
@@ -8,7 +9,32 @@ const generateNotes = function (notes) {
     <div class="card__item">
           <div class="card__title">
             <h4>${note.title}</h4>
-            <svg
+
+            <a 
+            onclick="selectedNote(${note.id})"
+            href="./add.html"
+            >
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="card__icon card__edit"
+              viewBox="0 0 512 512"
+              
+              >
+              <path
+                d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="32"
+              />
+              <path
+                d="M459.94 53.25a16.06 16.06 0 00-23.22-.56L424.35 65a8 8 0 000 11.31l11.34 11.32a8 8 0 0011.34 0l12.06-12c6.1-6.09 6.67-16.01.85-22.38zM399.34 90L218.82 270.2a9 9 0 00-2.31 3.93L208.16 299a3.91 3.91 0 004.86 4.86l24.85-8.35a9 9 0 003.93-2.31L422 112.66a9 9 0 000-12.66l-9.95-10a9 9 0 00-12.71 0z"
+              />
+            </svg>
+          </a>
+
+          <svg
               xmlns="http://www.w3.org/2000/svg"
               class="card__icon card__delete"
               viewBox="0 0 512 512"
@@ -23,6 +49,7 @@ const generateNotes = function (notes) {
                 d="M368 368L144 144M368 144L144 368"
               />
             </svg>
+
           </div>
           <p class="card__text">
           ${note.text}
@@ -31,6 +58,37 @@ const generateNotes = function (notes) {
     `;
     noteBox.innerHTML += html;
   });
+};
+
+const generateTags = function (notes) {
+  tagList.innerHTML = `
+    <li class="tags__item active">
+      <a class="tags__link" href="">#all</a>
+    </li>
+  `;
+  tagArr = [];
+  notes.forEach((note) => {
+    if (!tagArr.includes(note.tag) && note.tag) {
+      tagArr.push(note.tag);
+    }
+  });
+
+  tagArr.forEach((note) => {
+    html = `
+    <li onclick="showTags('${note}')" class="tags__item">
+      <a class="tags__link" href="#">${note}</a>
+    </li>
+    `;
+
+    tagList.innerHTML += html;
+  });
+};
+
+const showTags = function (noteTag) {
+  const notes = getLocalStorage();
+  const notesTag = notes.filter((note) => note.tag === noteTag);
+
+  generateNotes(notesTag);
 };
 
 const setLocalStorage = (notes) =>
@@ -45,6 +103,7 @@ const init = function () {
   const notes = hasLocalStorage ? hasLocalStorage : [];
   if (notes.length > 0) {
     generateNotes(notes);
+    generateTags(notes);
   } else {
     noteBox.innerHTML = "";
   }
@@ -61,3 +120,10 @@ const deleteNote = function (id) {
 };
 
 document.addEventListener("DOMContentLoaded", init);
+
+const selectedNote = function (id) {
+  const notes = getLocalStorage();
+  const selectedNote = notes.find((note) => note.id === id);
+
+  localStorage.setItem("selectedNote", JSON.stringify(selectedNote));
+};
